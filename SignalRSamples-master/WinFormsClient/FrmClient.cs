0b0cd408ee9a376a68a5570086a551d5ebeea473
2weeds <dovydas.zamas@gmail.com>
2021-09-27 12:29:11 +0300

    Corrections

---
+++ SignalRSamples-master/WinFormsClient/FrmClient.cs
@@ -0,0 +1,210 @@
+﻿using Microsoft.AspNet.SignalR.Client;
+using SgClient1;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace WinFormsClient
+{
+    public partial class FrmClient : Form
+    {
+        //Connection to a SignalR server
+        HubConnection _signalRConnection;
+        //Proxy object for a hub hosted on the SignalR server
+        IHubProxy _hubProxy;
+
+        public FrmClient()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnConnect_Click(object sender, EventArgs e)
+        {
+            await connectAsync();
+        }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            //Close the server connection if exists
+            if (_signalRConnection != null)
+            {
+                _signalRConnection.Stop();
+                _signalRConnection.Dispose();
+                _signalRConnection = null;
+
+                btnConnect.Enabled = true;
+                txtUrl.Enabled = true;
+                txtUserName.Enabled = true;
+                btnDisconnect.Enabled = false;
+                grpMessaging.Enabled = false;
+                grpMembership.Enabled = false;
+
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            //Call the "Send" method on the hub (on the server) with the given parameters
+            _hubProxy.Invoke("Send", txtMessage.Text);
+        }
+
+        private void btnJoinGroup_Click(object sender, EventArgs e)
+        {
+            //Call the "JoinGroup" method on the hub (on the server)
+            _hubProxy.Invoke("JoinGroup", txtGroupName.Text);
+        }
+
+        private void btnLeaveGroup_Click(object sender, EventArgs e)
+        {
+            //Call the "LeaveGroup" method on the hub (on the server)
+            _hubProxy.Invoke("LeaveGroup", txtGroupName.Text);
+        }
+
+        private async Task connectAsync()
+        {
+            //Create a connection for the SignalR server
+            _signalRConnection = new HubConnection(txtUrl.Text);
+            _signalRConnection.StateChanged += HubConnection_StateChanged;
+
+            //Get a proxy object that will be used to interact with the specific hub on the server
+            //Ther may be many hubs hosted on the server, so provide the type name for the hub
+            _hubProxy = _signalRConnection.CreateHubProxy("SimpleHub");
+
+            //Reigster to the "AddMessage" callback method of the hub
+            //This method is invoked by the hub
+            _hubProxy.On<string, string>("AddMessage", (name, message) => writeToLog($"{name}:{message}"));
+            _hubProxy.On<int>("updateMembers", (size) => updateGroupSize(size));
+            _hubProxy.On<int>("updateReady", (size) => updateReadyCheckSize(size));
+            _hubProxy.On<int>("getReadyPlayers", (size) => triggerGameForm(size));
+
+
+            btnConnect.Enabled = false;
+
+            try
+            {
+                //Connect to the server
+                await _signalRConnection.Start();
+
+                //Send user name for this client, so we won't need to send it with every message
+                await _hubProxy.Invoke("SetUserName", txtUserName.Text);
+
+                txtUrl.Enabled = false;
+                txtUserName.Enabled = false;
+                btnDisconnect.Enabled = true;
+                grpMessaging.Enabled = true;
+                grpMembership.Enabled = true;
+                grpServer1.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                writeToLog($"Error:{ex.Message}");
+                btnConnect.Enabled = true;
+            }
+        }
+
+        private void HubConnection_StateChanged(StateChange obj)
+        {
+            if (obj.NewState == Microsoft.AspNet.SignalR.Client.ConnectionState.Connected)
+                writeToLog("Connected");
+            else if (obj.NewState == Microsoft.AspNet.SignalR.Client.ConnectionState.Disconnected)
+                writeToLog("Disconnected");
+        }
+
+        private void writeToLog(string log)
+        {
+            if (this.InvokeRequired)
+                this.BeginInvoke(new Action(() => txtLog.AppendText(log + Environment.NewLine)));
+            else
+                txtLog.AppendText(log + Environment.NewLine);
+        }
+
+        private void triggerGameForm(int size)
+        {
+            if (size == 2)
+            {
+                // FormGame gamefrm = new FormGame();
+                if (this.InvokeRequired)
+                {
+                    this.BeginInvoke(new Action(() => btnPlay.Visible = true));
+                    this.BeginInvoke(new Action(() => readyServerButton1.Visible = false));
+                }
+                else
+                {
+                    btnPlay.Visible = true;
+                    readyServerButton1.Visible = false;
+                }
+            }
+        }
+
+        private void updateGroupSize(int size)
+        {
+            if (this.InvokeRequired)
+                this.BeginInvoke(new Action(() => labelServerPlayers.Text = "Players: " + size.ToString() + "/2"));
+            else
+                labelServerPlayers.Text = "Players: " + size.ToString() + "/2";
+        }
+
+        private void updateReadyCheckSize(int size)
+        {
+            if (this.InvokeRequired)
+                this.BeginInvoke(new Action(() => labelReadyServer1.Text = "Ready: " + size.ToString() + "/2"));
+            else
+                labelReadyServer1.Text = "Ready: " + size.ToString() + "/2";
+        }
+
+        private void FrmClient_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void joinServerButton1_Click(object sender, EventArgs e)
+        {
+            _hubProxy.Invoke("JoinGroup", grpServer1.Text);
+            readyServerButton1.Visible = true;
+            joinServerButton1.Visible = false;
+            readyServerButton1.Enabled = true;
+            joinServerButton1.Enabled = false;
+        }
+
+        private void leaveServerButton1_Click(object sender, EventArgs e)
+        {
+            _hubProxy.Invoke("LeaveGroup", grpServer1.Text);
+            labelServerPlayers.Text = "Players 0/2";
+            labelReadyServer1.Text = "Ready 0/2";
+            joinServerButton1.Visible = true;
+            joinServerButton1.Enabled = true;
+            readyServerButton1.Visible = false;
+            readyServerButton1.Enabled = false;
+            btnPlay.Visible = false;
+            btnPlay.Enabled = false;
+
+        }
+
+        private void readyServerButton1_Click(object sender, EventArgs e)
+        {
+            _hubProxy.Invoke("ReadyCheck", grpServer1.Text);
+            readyServerButton1.Enabled = false;
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            _hubProxy.Invoke("ResetReadyCheck", grpServer1.Text);
+            readyServerButton1.Visible = true;
+            readyServerButton1.Enabled = true;
+            btnPlay.Visible = false;
+            btnPlay.Enabled = false;
+            FormGame gamefrm = new FormGame();
+            gamefrm.Show();
+        }
+    }
+}
---
+++ SignalRSamples-master/WinFormsServer/FrmServer.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Owin;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.Hosting;
+using Microsoft.AspNet.SignalR;
+using System.ComponentModel;
+using System.Collections.Concurrent;
+
+namespace WinFormsServer
+{
+    public partial class FrmServer : Form
+    {
+        private IDisposable _signalR;
+        private BindingList<ClientItem> _clients = new BindingList<ClientItem>();
+        private BindingList<string> _groups = new BindingList<string>();
+        private ConcurrentDictionary<string, int> _groupsCount = new ConcurrentDictionary<string, int>();
+        private ConcurrentDictionary<string, int> _readyCount = new ConcurrentDictionary<string, int>();
+
+        public FrmServer()
+        {
+            InitializeComponent();
+
+            bindListsToControls();
+
+            //Register to static hub events
+            SimpleHub.ClientConnected += SimpleHub_ClientConnected;
+            SimpleHub.ClientDisconnected += SimpleHub_ClientDisconnected;
+            SimpleHub.ClientNameChanged += SimpleHub_ClientNameChanged;
+            SimpleHub.ClientJoinedToGroup += SimpleHub_ClientJoinedToGroup;
+            SimpleHub.ClientReadyCheck += SimpleHub_ClientReadyCheck;
+            SimpleHub.ResetClientReadyCheck += SimpleHub_ResetClientReadyCheck;
+            SimpleHub.ClientLeftGroup += SimpleHub_ClientLeftGroup;
+            SimpleHub.MessageReceived += SimpleHub_MessageReceived;
+        }
+
+        private void bindListsToControls()
+        {
+            //Clients list
+            cmbClients.DisplayMember = "Name";
+            cmbClients.ValueMember = "Id";
+            cmbClients.DataSource = _clients;
+
+            //Groups list
+            cmbGroups.DataSource = _groups;
+        }
+
+        private void SimpleHub_ClientConnected(string clientId)
+        {
+            //Add client to our clients list
+            this.BeginInvoke(new Action(() => _clients.Add(new ClientItem() { Id = clientId, Name = clientId })));
+
+            writeToLog($"Client connected:{clientId}");
+        }
+
+        private void SimpleHub_ClientDisconnected(string clientId)
+        {
+            //Remove client from the list
+            this.BeginInvoke(new Action(() =>
+            {
+                var client = _clients.FirstOrDefault(x => x.Id == clientId);
+                if (client != null)
+                    _clients.Remove(client);
+            }));
+
+            writeToLog($"Client disconnected:{clientId}");
+        }
+
+        private void SimpleHub_ClientNameChanged(string clientId, string newName)
+        {
+            //Update the client's name if it exists
+            this.BeginInvoke(new Action(() =>
+            {
+                var client = _clients.FirstOrDefault(x => x.Id == clientId);
+                if (client != null)
+                    client.Name = newName;
+            }));
+
+            writeToLog($"Client name changed. Id:{clientId}, Name:{newName}");
+        }
+
+        private void SimpleHub_ClientJoinedToGroup(string clientId, string groupName)
+        {
+            //Only add the groups name to our groups list
+            this.BeginInvoke(new Action(() =>
+            {
+                int groupSize;
+                var group = _groups.FirstOrDefault(x => x == groupName);
+                if (group == null)
+                {
+                    _groups.Add(groupName);
+                    _groupsCount.TryAdd(groupName, +1);
+                }
+                else
+                {
+                    _groupsCount.AddOrUpdate(groupName, 1, (groupname, count) => count + 1);
+                }
+                //_groupsCount.TryGetValue(groupName, out groupSize);
+                _groupsCount.TryGetValue(groupName, out groupSize);
+                var hubContext = GlobalHost.ConnectionManager.GetHubContext<SimpleHub>();
+                hubContext.Clients.Group(groupName).updateMembers(groupSize);
+
+            }));
+            writeToLog($"Client joined to group. Id:{clientId}, Group:{groupName}");
+        }
+
+        private void SimpleHub_ClientReadyCheck(string clientId, string groupName)
+        {
+            this.BeginInvoke(new Action(() =>
+            {
+                int groupSize;
+                var group = _groups.FirstOrDefault(x => x == groupName);
+                if (group == null)
+                {
+                    _readyCount.TryAdd(groupName, +1);
+                }
+                else
+                {
+                    _readyCount.AddOrUpdate(groupName, 1, (groupname, count) => count + 1);
+                }
+                //_groupsCount.TryGetValue(groupName, out groupSize);
+                _readyCount.TryGetValue(groupName, out groupSize);
+                var hubContext = GlobalHost.ConnectionManager.GetHubContext<SimpleHub>();
+                hubContext.Clients.Group(groupName).updateReady(groupSize);
+                hubContext.Clients.Group(groupName).getReadyPlayers(groupSize);
+            }));
+            writeToLog($"Client is ready. Id:{clientId}, Group:{groupName}");
+        }
+
+        private void SimpleHub_ResetClientReadyCheck(string clientId, string groupName)
+        {
+            this.BeginInvoke(new Action(() =>
+            {
+                 int lastReadySize;
+                _readyCount.TryRemove(groupName, out lastReadySize);
+                var hubContext = GlobalHost.ConnectionManager.GetHubContext<SimpleHub>();
+                hubContext.Clients.Group(groupName).updateReady(0);
+            }));
+            writeToLog($"Resetting ready checks. Initiated by client: Id:{clientId}, Group:{groupName}");
+        }
+
+        private void SimpleHub_ClientLeftGroup(string clientId, string groupName)
+        {
+            this.BeginInvoke(new Action(() =>
+            {
+                int groupSize;
+                int checkSize;
+                var group = _groups.FirstOrDefault(x => x == groupName);
+                _groupsCount.TryGetValue(groupName, out groupSize);
+                if (group != null && groupSize > 1)
+                {
+                    _groupsCount.AddOrUpdate(groupName, 1, (groupname, count) => count - 1);
+                    _readyCount.TryGetValue(groupName, out checkSize);
+                    if (checkSize > 0)
+                    {
+                        _readyCount.AddOrUpdate(groupName, 1, (groupname, count) => count - 1);
+                    }
+                    _groupsCount.TryGetValue(groupName, out groupSize);
+                    var hubContext = GlobalHost.ConnectionManager.GetHubContext<SimpleHub>();
+                    hubContext.Clients.Group(groupName).updateMembers(groupSize);
+                }
+                else if (group != null && groupSize == 1)
+                {
+                    int lastSize;
+                    int lastReadySize;
+                    _groups.Remove(groupName);
+                    _groupsCount.TryRemove(groupName, out lastSize);
+                    _readyCount.TryRemove(groupName, out lastReadySize);
+                }
+
+            }));
+            writeToLog($"Client left group. Id:{clientId}, Group:{groupName}");
+        }
+
+        private void SimpleHub_MessageReceived(string senderClientId, string message)
+        {
+            //One of the clients sent a message, log it
+            this.BeginInvoke(new Action(() =>
+            {
+                string clientName = _clients.FirstOrDefault(x => x.Id == senderClientId)?.Name;
+
+                writeToLog($"{clientName}:{message}");
+            }));
+        }
+
+        private void btnStartServer_Click(object sender, EventArgs e)
+        {
+            txtLog.Clear();
+
+            try
+            {
+                //Start SignalR server with the give URL address
+                //Final server address will be "URL/signalr"
+                //Startup.Configuration is called automatically
+                _signalR = WebApp.Start<Startup>(txtUrl.Text);
+
+                btnStartServer.Enabled = false;
+                txtUrl.Enabled = false;
+                btnStop.Enabled = true;
+                grpBroadcast.Enabled = true;
+
+                writeToLog($"Server started at:{txtUrl.Text}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            _clients.Clear();
+            _groups.Clear();
+
+            SimpleHub.ClearState();
+
+            if (_signalR != null)
+            {
+                _signalR.Dispose();
+                _signalR = null;
+
+                btnStop.Enabled = false;
+                btnStartServer.Enabled = true;
+                txtUrl.Enabled = true;
+                grpBroadcast.Enabled = false;
+
+                writeToLog("Server stopped.");
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            var hubContext = GlobalHost.ConnectionManager.GetHubContext<SimpleHub>();
+
+            if (rdToAll.Checked)
+            {
+                hubContext.Clients.All.addMessage("SERVER", txtMessage.Text);
+            }
+            else if (rdToGroup.Checked)
+            {
+                hubContext.Clients.Group(cmbGroups.Text).addMessage("SERVER", txtMessage.Text);
+            }
+            else if (rdToClient.Checked)
+            {
+                hubContext.Clients.Client((string)cmbClients.SelectedValue).addMessage("SERVER", txtMessage.Text);
+            }
+        }
+
+        private void writeToLog(string log)
+        {
+            if (this.InvokeRequired)
+                this.BeginInvoke(new Action(() => txtLog.AppendText(log + Environment.NewLine)));
+            else
+                txtLog.AppendText(log + Environment.NewLine);
+        }
+
+        private void FrmServer_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
